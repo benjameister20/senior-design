b16ea0ce96a553efd551db77c677b6ca97b6cfb8
Trevor Stevenson <trevor.stevenson@duke.edu>
2020-01-25 02:28:06 -0500

    Fix imports

--- ReactAndFlask/flask-backend/application.py
+++ ReactAndFlask/flask-backend/application.py
@@ -33,6 +33,11 @@ def index():
     return render_template("index.html")
 
 
+@application.route("/test")
+def test():
+    return "testing"
+
+
 def _register_routes() -> None:
     """
     Register routes

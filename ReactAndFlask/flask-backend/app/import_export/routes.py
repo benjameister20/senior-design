dde2a599a4e6690eb97dac79f48c60d80cf87fca
Trevor Stevenson <trevor.stevenson@duke.edu>
2020-02-01 21:01:05 -0500

    Add initial route

--- ReactAndFlask/flask-backend/app/import_export/routes.py
+++ ReactAndFlask/flask-backend/app/import_export/routes.py
@@ -1,6 +1,8 @@
+import csv
+import io
 from http import HTTPStatus
 
-from flask import Blueprint
+from flask import Blueprint, request
 
 import_export = Blueprint("import_export", __name__)
 
@@ -8,4 +10,17 @@ import_export = Blueprint("import_export", __name__)
 @import_export.route("/import", methods=["POST"])
 def import_csv():
     """ Bulk import from a csv file """
+    f = request.files["file"]
+    if not f:
+        return HTTPStatus.BAD_REQUEST
+
+    stream: io.StringIO = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
+    csv_input = csv.reader(stream)
+
+    for row in csv_input:
+        print(row)
+
+    stream.seek(0)
+    stream.read()
+
     return HTTPStatus.OK

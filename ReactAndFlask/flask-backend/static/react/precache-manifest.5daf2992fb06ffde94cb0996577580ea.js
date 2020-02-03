dff0e3cea6902c7f01d1d1107f0ca81649a157cb
Connor Ghazaleh <cfg11@duke.edu>
2020-02-03 14:32:39 -0500

    rebuilt

--- ReactAndFlask/flask-backend/static/react/precache-manifest.5daf2992fb06ffde94cb0996577580ea.js
+++ ReactAndFlask/flask-backend/static/react/precache-manifest.5daf2992fb06ffde94cb0996577580ea.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d31b0e90299777b9f17774994a5f70a7",
+    "revision": "e1a9a3558675e0d2f18f8e875cd493fe",
     "url": "/static/react/../../templates/index.html"
   },
   {
-    "revision": "c6f67aaa51bba40cb051",
+    "revision": "a032ada4d1f6dc526e36",
     "url": "/static/react/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "08083fdb37ea99750492",
-    "url": "/static/react/js/2.5ad26d98.chunk.js"
+    "revision": "f075815f15cf388f4ead",
+    "url": "/static/react/js/2.555dd640.chunk.js"
   },
   {
     "revision": "3d416322b0ec4a5a91d5be4afe7c848c",
-    "url": "/static/react/js/2.5ad26d98.chunk.js.LICENSE"
+    "url": "/static/react/js/2.555dd640.chunk.js.LICENSE"
   },
   {
-    "revision": "c6f67aaa51bba40cb051",
-    "url": "/static/react/js/main.44427e79.chunk.js"
+    "revision": "a032ada4d1f6dc526e36",
+    "url": "/static/react/js/main.5961d73a.chunk.js"
   },
   {
     "revision": "1c67bf2225e5a0f28767",
--- ReactAndFlask/flask-backend/static/react/service-worker.js
+++ ReactAndFlask/flask-backend/static/react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/static/react/precache-manifest.5e4d09cdf510b191a01cd33b2bfd41ff.js"
+  "/static/react/precache-manifest.5daf2992fb06ffde94cb0996577580ea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/static/react/index.html"), {
 
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

175e4d7c1189473537da0c9aa76880aa7191f703
Eric Werbel <enw12@duke.edu>
2020-02-09 17:11:37 -0500

    Added constants file

---
+++ ReactAndFlask/flask-backend/app/constants.py
@@ -0,0 +1,50 @@
+class Constants:
+
+    API_SUCCESS = "success"
+    COMMENT_KEY = "comment"
+    RESULTS_KEY = "results"
+
+    DATACENTER_KEY = "datacenter_id"
+
+    # User Keys
+    USERNAME_KEY = "username"
+    PASSWORD_KEY = "password"
+    EMAIL_KEY = "email"
+    DISPLAY_NAME_KEY = "display_name"
+    PRIVILEGE_KEY = "privilege"
+
+    # Model Keys
+    VENDOR_KEY = "vendor"
+    MODEL_NUMBER_KEY = "model_number"
+    HEIGHT_KEY = "height"
+    DISPLAY_COLOR_KEY = "display_color"
+    ETHERNET_PORT_KEY = "ethernet_ports"
+    POWER_PORT_KEY = "power_ports"
+    CPU_KEY = "cpu"
+    MEMORY_KEY = "memory"
+    STORAGE_KEY = "storage"
+
+    VENDOR_ORIG_KEY = "vendorOriginal"
+    MODEL_NUMBER_ORIG_KEY = "model_numberOriginal"
+    HEIGHT_ORIG_KEY = "heightOriginal"
+
+    # Asset Keys
+    MODEL_KEY = "model"
+    HOSTNAME_KEY = "hostname"
+    RACK_KEY = "rack"
+    RACK_POSITION_KEY = "rack_position"
+    OWNER_KEY = "owner"
+    TAGS_KEY = "tags"
+    MAC_ADDRESS_KEY = "mac_address"
+    NETWORK_CONNECTIONS_KEY = "network_connections"
+    POWER_CONNECTIONS_KEY = "power_connections"
+    ASSET_NUMBER_KEY = "asset_number"
+
+    ASSET_NUMBER_ORIG_KEY = "asset_numberOriginal"
+
+    # Rack Keys
+    START_LETTER_KEY = "start_letter"
+    START_NUMBER_KEY = "start_number"
+    STOP_LETTER_KEY = "stop_letter"
+    STOP_NUMBER_KEY = "stop_number"
+    RACK_HEIGHT = 42

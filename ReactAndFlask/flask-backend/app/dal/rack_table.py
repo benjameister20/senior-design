c80752a7accb7d3072192808443268c1df59a82c
Eric Werbel <enw12@duke.edu>
2020-02-13 22:36:02 -0500

    Added primary key to rack table

--- ReactAndFlask/flask-backend/app/dal/rack_table.py
+++ ReactAndFlask/flask-backend/app/dal/rack_table.py
@@ -8,6 +8,7 @@ from sqlalchemy.exc import IntegrityError
 class RackEntry(db.Model):
     __tablename__ = "racks"
 
+    identifier = db.Column(db.Integer, primary_key=True, unique=True)
     label = db.Column(db.String(80))
     datacenter_id = db.Column(db.Integer)
 

c23f85c11378a213d493d8c044a20baddd4376ef
Connor Ghazaleh <cfg11@duke.edu>
2020-02-19 20:32:24 -0500

    added logging for racks, datacenters; added user field to log messages and log entries; added auto-create log file if it doesn't exist

--- ReactAndFlask/flask-backend/app/decorators/logs.py
+++ ReactAndFlask/flask-backend/app/decorators/logs.py
@@ -1,19 +1,38 @@
 from functools import wraps
 
+from app.exceptions.UserExceptions import UserException
 from app.logging.logger import Logger
+from app.users.authentication import AuthManager
 
 LOGGER = Logger()
+AUTHMANAGER = AuthManager()
 
 
 def log(request, resource, action):
     def wrap(f):
         @wraps(f)
         def wrapped(*args, **kwargs):
-            # print("Before decorated function")
-            LOGGER.log_request(request.get_json(), resource, action)
+            print("ACTION")
+            print(action)
+            token = request.headers.get("token")
+            user = ""
+            try:
+                if action not in [
+                    Logger.ACTIONS.USERS.AUTHENTICATE,
+                    Logger.ACTIONS.USERS.OAUTH,
+                ]:
+                    user = AUTHMANAGER.decode_auth_token(token)
+                else:
+                    user = request.get_json().get("username")
+            except UserException as e:
+                return e.message
+
+            print("USER")
+            print(user)
+
+            LOGGER.log_request(request.get_json(), resource, action, user)
             response = f(*args, **kwargs)
             LOGGER.log_response(response)
-            # print("After decorated function")
             return response
 
         return wrapped
--- ReactAndFlask/flask-backend/app/logging/logger.py
+++ ReactAndFlask/flask-backend/app/logging/logger.py
@@ -36,203 +36,138 @@ class RackActions:
     DELETE = "DELETE"
 
 
+class DataCenterActions:
+    CREATE = "CREATE"
+    DELETE = "DELETE"
+
+
 class Actions:
     USERS = UserActions()
     MODELS = ModelActions()
     INSTANCES = InstanceActions()
     RACKS = RackActions()
+    DATACENTERS = DataCenterActions()
 
 
 class Logger:
 
-    MODELS = "model"
-    INSTANCES = "asset"
-    RACKS = "rack"
+    MODELS = "models"
+    INSTANCES = "assets"
+    RACKS = "racks"
     USERS = "users"
+    DATACENTERS = "datacenters"
     ACTIONS = Actions()
 
     def __init__(self):
         self.logfile = "/app.log"
         self.dirname = os.path.dirname(__file__)
         self.LOG = {}
+        logfile_template = {"log": []}
+        logfile_exists = os.path.exists(self.dirname + self.logfile)
+        if not logfile_exists:
+            with open(self.dirname + self.logfile, "w") as lf:
+                json.dump(logfile_template, lf, indent=4)
+
         with open(self.dirname + self.logfile, "r") as infile:
             self.LOG = json.load(infile)
 
     @staticmethod
     def __add_timestamp(log_message):
-        log_message = f"[{str(datetime.now())}] - {log_message}"
+        log_message = f"[{str(datetime.now())}] {log_message}"
 
         return log_message
 
-    def __log_user_request(self, request, action):
-        # username = request.get(Constants.USERNAME_KEY)
-        # old_username = request.get(Constants.OLD_USERNAME_KEY)
-        # email = request.get(Constants.EMAIL_KEY)
-        # display_name = request.get(Constants.DISPLAY_NAME_KEY)
-        # privilege = request.get(Constants.PRIVILEGE_KEY)
+    def __log_user_request(self, action):
 
         if action == Logger.ACTIONS.USERS.CREATE:
-            log_message = f"""CREATE user (
-                Username: <<{Constants.USERNAME_KEY}>>
-                Email: <<{Constants.EMAIL_KEY}>>
-                Display Name: <<{Constants.DISPLAY_NAME_KEY}>>
-                Privilege: <<{Constants.PRIVILEGE_KEY}>>
-                )"""
+            log_message = f"""CREATE User (Username: <<{Constants.USERNAME_KEY}>>, Email: <<{Constants.EMAIL_KEY}>>, Display Name: <<{Constants.DISPLAY_NAME_KEY}>>, Privilege: <<{Constants.PRIVILEGE_KEY}>>)"""
 
         if action == Logger.ACTIONS.USERS.EDIT:
-            log_message = f"""EDIT user (
-                Username: <<{Constants.OLD_USERNAME_KEY}>>
-                ) becomes (
-                Username: <<{Constants.USERNAME_KEY}>>
-                Email: <<{Constants.EMAIL_KEY}>>
-                Display Name: <<{Constants.DISPLAY_NAME_KEY}>>
-                Privilege: <<{Constants.PRIVILEGE_KEY}>>
-                )"""
+            log_message = f"""EDIT User (Username: <<{Constants.OLD_USERNAME_KEY}>>) becomes (Username: <<{Constants.USERNAME_KEY}>>, Email: <<{Constants.EMAIL_KEY}>>, Display Name: <<{Constants.DISPLAY_NAME_KEY}>>, Privilege: <<{Constants.PRIVILEGE_KEY}>>)"""
             # log_syntax = "EDIT user (<<old_username>>) to (<<username>>, <<email>>, <<display_name>>, <<privilege>>)"
 
         if action == Logger.ACTIONS.USERS.DELETE:
-            log_message = f"""DELETE user (
-                Username: <<{Constants.USERNAME_KEY}>>
-                )"""
+            log_message = f"""DELETE User (Username: <<{Constants.USERNAME_KEY}>>)"""
             # log_syntax = "DELETE user (<<username>>)"
 
         if action == Logger.ACTIONS.USERS.AUTHENTICATE:
-            log_message = f"""AUTHENTICATE user (
-                Username: <<{Constants.USERNAME_KEY}>>
-                )"""
+            log_message = (
+                f"""AUTHENTICATE User (Username: <<{Constants.USERNAME_KEY}>>)"""
+            )
             # log_syntax = "AUTHENTICATE user (<<username>>)"
 
         if action == Logger.ACTIONS.USERS.LOGOUT:
-            log_message = f"""LOGOUT user (
-                Username: <<{Constants.USERNAME_KEY}>>
-                )"""
+            log_message = f"""LOGOUT User (Username: <<{Constants.USERNAME_KEY}>>)"""
             # log_syntax = "LOGOUT user (<<username>>)"
 
         if action == Logger.ACTIONS.USERS.OAUTH:
-            log_message = f"""OAUTH user (
-                Username: <<{Constants.USERNAME_KEY}>>
-                )"""
+            log_message = f"""OAUTH User (Username: <<{Constants.USERNAME_KEY}>>)"""
             # log_syntax = "OAUTH user (<<username>>)"
 
         return log_message
 
-    def __log_instance_request(self, request, action):
-        # original_asset_number = request.get(Constants.ASSET_NUMBER_ORIG_KEY)
-        # model_name = request.get(Constants.MODEL_KEY)
-        # datacenter_name = request.get(Constants.DATACENTER_KEY)
-        # hostname = request.get(Constants.HOSTNAME_KEY)
-        # rack = request.get(Constants.RACK_KEY)
-        # rack_position = request.get(Constants.RACK_POSITION_KEY)
-        # owner = request.get(Constants.OWNER_KEY)
-        # comment = request.get(Constants.COMMENT_KEY)
-        # mac_address = request.get(Constants.MAC_ADDRESS_KEY)
-        # network_connections = request.get(
-        #     Constants.NETWORK_CONNECTIONS_KEY
-        # )
-        # power_connections = request.get(
-        #     Constants.POWER_CONNECTIONS_KEY
-        # )
-        # asset_number = request.get(Constants.ASSET_NUMBER_KEY)
+    def __log_instance_request(self, action):
 
         if action == Logger.ACTIONS.INSTANCES.CREATE:
-            log_message = f"""CREATE asset (
-                Model Name: <<{Constants.MODEL_KEY}>>
-                Datacenter Name: <<{Constants.DATACENTER_KEY}>>
-                Hostname: <<{Constants.HOSTNAME_KEY}>>
-                Rack: <<{Constants.RACK_KEY}>>
-                Rack Position: <<{Constants.RACK_POSITION_KEY}>>
-                Owner: <<{Constants.OWNER_KEY}>>
-                Comment: <<{Constants.COMMENT_KEY}>>
-                MAC Address: <<{Constants.MAC_ADDRESS_KEY}>>
-                Asset Number: <<{Constants.ASSET_NUMBER_KEY}>>
-                )"""
+            log_message = f"""CREATE Asset (Model Name: <<{Constants.MODEL_KEY}>>, Datacenter Name: <<{Constants.DC_NAME_KEY}>>, Hostname: <<{Constants.HOSTNAME_KEY}>>, Rack: <<{Constants.RACK_KEY}>>, Rack Position: <<{Constants.RACK_POSITION_KEY}>>, Owner: <<{Constants.OWNER_KEY}>>, Comment: <<{Constants.COMMENT_KEY}>>, MAC Address: <<{Constants.MAC_ADDRESS_KEY}>>, Asset Number: <<{Constants.ASSET_NUMBER_KEY}>>)"""
             # log_syntax = """CREATE instance (
             #     <<model_name>>, <<email>>, <<display_name>>, <<privilege>>)"""
 
         if action == Logger.ACTIONS.INSTANCES.EDIT:
-            log_message = f"""EDIT asset (
-                Asset Number: <<{Constants.ASSET_NUMBER_ORIG_KEY}>>
-                ) becomes: (
-                Model Name: <<{Constants.MODEL_KEY}>>
-                Datacenter Name: <<{Constants.DATACENTER_KEY}>>
-                Hostname: <<{Constants.HOSTNAME_KEY}>>
-                Rack: <<{Constants.RACK_KEY}>>
-                Rack Position: <<{Constants.RACK_POSITION_KEY}>>
-                Owner: <<{Constants.OWNER_KEY}>>
-                Comment: <<{Constants.COMMENT_KEY}>>
-                MAC Address: <<{Constants.MAC_ADDRESS_KEY}>>
-                Asset Number: <<{Constants.ASSET_NUMBER_KEY}>>
-                )"""
+            log_message = f"""EDIT Asset (Model Name: <<{Constants.MODEL_KEY}>>, Datacenter Name: <<{Constants.DC_NAME_KEY}>>, Hostname: <<{Constants.HOSTNAME_KEY}>>, Rack: <<{Constants.RACK_KEY}>>, Rack Position: <<{Constants.RACK_POSITION_KEY}>>, Owner: <<{Constants.OWNER_KEY}>>, Comment: <<{Constants.COMMENT_KEY}>>, MAC Address: <<{Constants.MAC_ADDRESS_KEY}>>, Asset Number: <<{Constants.ASSET_NUMBER_KEY}>>)"""
             # log_syntax = "EDIT instance (<<old_username>>) to (<<username>>, <<email>>, <<display_name>>, <<privilege>>)"
 
         if action == Logger.ACTIONS.INSTANCES.DELETE:
-            log_message = f"""DELETE asset (
-                Model Name: <<{Constants.MODEL_KEY}>>
-                Datacenter Name: <<{Constants.DATACENTER_KEY}>>
-                Hostname: <<{Constants.HOSTNAME_KEY}>>
-                )"""
+            log_message = f"""DELETE Asset (Model Name: <<{Constants.MODEL_KEY}>>, Datacenter Name: <<{Constants.DC_NAME_KEY}>>, Hostname: <<{Constants.HOSTNAME_KEY}>>)"""
             # log_syntax = "DELETE user (<<username>>)"
 
         return log_message
 
-    def __log_model_request(self, request, action):
+    def __log_model_request(self, action):
         if action == Logger.ACTIONS.MODELS.CREATE:
-            log_message = f"""CREATE model (
-                Vendor: <<{Constants.VENDOR_KEY}>>
-                Model Number: <<{Constants.MODEL_NUMBER_KEY}>>
-                Height: <<{Constants.HEIGHT_KEY}>>
-                Display Color: <<{Constants.DISPLAY_COLOR_KEY}>>
-                Ethernet Ports: <<{Constants.ETHERNET_PORT_KEY}>>
-                Power Ports: <<{Constants.POWER_PORT_KEY}>>
-                CPU: <<{Constants.CPU_KEY}>>
-                Memory: <<{Constants.MEMORY_KEY}>>
-                Storage: <<{Constants.STORAGE_KEY}>>
-                Comment: <<{Constants.COMMENT_KEY}>>
-                )"""
+            log_message = f"""CREATE Model (Vendor: <<{Constants.VENDOR_KEY}>>, Model Number: <<{Constants.MODEL_NUMBER_KEY}>>, Height: <<{Constants.HEIGHT_KEY}>>, Display Color: <<{Constants.DISPLAY_COLOR_KEY}>>, Ethernet Ports: <<{Constants.ETHERNET_PORT_KEY}>>, Power Ports: <<{Constants.POWER_PORT_KEY}>>, CPU: <<{Constants.CPU_KEY}>>, Memory: <<{Constants.MEMORY_KEY}>>, Storage: <<{Constants.STORAGE_KEY}>>, Comment: <<{Constants.COMMENT_KEY}>>)"""
             # log_syntax = "CREATE user (<<username>>, <<email>>, <<display_name>>, <<privilege>>)"
 
         if action == Logger.ACTIONS.MODELS.EDIT:
-            log_message = f"""EDIT model (
-                Vendor: <<{Constants.VENDOR_KEY}>>
-                Model Number: <<{Constants.MODEL_NUMBER_KEY}>>
-                ) becomes: (
-                Vendor: <<{Constants.VENDOR_KEY}>>
-                Model Number: <<{Constants.MODEL_NUMBER_KEY}>>
-                Height: <<{Constants.HEIGHT_KEY}>>
-                Display Color: <<{Constants.DISPLAY_COLOR_KEY}>>
-                Ethernet Ports: <<{Constants.ETHERNET_PORT_KEY}>>
-                Power Ports: <<{Constants.POWER_PORT_KEY}>>
-                CPU: <<{Constants.CPU_KEY}>>
-                Memory: <<{Constants.MEMORY_KEY}>>
-                Storage: <<{Constants.STORAGE_KEY}>>
-                Comment: <<{Constants.COMMENT_KEY}>>
-                )"""
+            log_message = f"""EDIT Model (Vendor: <<{Constants.VENDOR_KEY}>>, Model Number: <<{Constants.MODEL_NUMBER_KEY}>>) becomes: (Vendor: <<{Constants.VENDOR_KEY}>>, Model Number: <<{Constants.MODEL_NUMBER_KEY}>>, Height: <<{Constants.HEIGHT_KEY}>>, Display Color: <<{Constants.DISPLAY_COLOR_KEY}>>, Ethernet Ports: <<{Constants.ETHERNET_PORT_KEY}>>, Power Ports: <<{Constants.POWER_PORT_KEY}>>, CPU: <<{Constants.CPU_KEY}>>, Memory: <<{Constants.MEMORY_KEY}>>, Storage: <<{Constants.STORAGE_KEY}>>, Comment: <<{Constants.COMMENT_KEY}>>)"""
             # log_syntax = "EDIT user (<<old_username>>) to (<<username>>, <<email>>, <<display_name>>, <<privilege>>)"
 
         if action == Logger.ACTIONS.MODELS.DELETE:
-            log_message = f"""DELETE model (
-                Vendor: <<{Constants.VENDOR_KEY}>>
-                Model Number: <<{Constants.MODEL_NUMBER_KEY}>>
-                )"""
+            log_message = f"""DELETE Model (Vendor: <<{Constants.VENDOR_KEY}>>, Model Number: <<{Constants.MODEL_NUMBER_KEY}>>)"""
             # log_syntax = "DELETE user (<<username>>)"
 
         return log_message
 
-    def __log_rack_request(self, request, action):
+    def __log_rack_request(self, action):
+        if action == Logger.ACTIONS.RACKS.CREATE:
+            log_message = f"""CREATE Rack range <<{Constants.START_LETTER_KEY}>>-<<{Constants.STOP_LETTER_KEY}>>, <<{Constants.START_NUMBER_KEY}>>-<<{Constants.STOP_NUMBER_KEY}>> in Datacenter <<{Constants.DC_NAME_KEY}>>"""
 
-        return ""
+        if action == Logger.ACTIONS.RACKS.DELETE:
+            log_message = f"""CREATE Rack range <<{Constants.START_LETTER_KEY}>>-<<{Constants.STOP_LETTER_KEY}>>, <<{Constants.START_NUMBER_KEY}>>-<<{Constants.STOP_NUMBER_KEY}>> from Datacenter <<{Constants.DC_NAME_KEY}>>"""
 
-    def __create_log_entry_request(self, request, log_message):
+        return log_message
+
+    def __log_datacenter_request(self, action):
+
+        if action == Logger.ACTIONS.DATACENTERS.CREATE:
+            log_message = f"""CREATE Datacenter (Name: {Constants.DC_NAME_KEY}, Abbreviation: {Constants.DC_ABRV_KEY})"""
+
+        if action == Logger.ACTIONS.DATACENTERS.DELETE:
+            log_message = f"""DELETE Datacenter (Name: {Constants.DC_NAME_KEY}, Abbreviation: {Constants.DC_ABRV_KEY})"""
+
+        return log_message
+
+    def __create_log_entry_request(self, request, resource, log_message, user):
         self.__refresh()
         timestamp = str(datetime.now())
-        message = f"[{timestamp}] - {log_message}"
+        message = f"[{timestamp}] {user} - {log_message}"
         if Constants.PASSWORD_KEY in request.keys():
-            request[Constants.PASSWORD_KEY] = "*" * len(
-                request.get(Constants.PASSWORD_KEY)
-            )
+            request[Constants.PASSWORD_KEY] = "*" * 12
         log_entry = {
             "timestamp": timestamp,
+            "user": user,
             "message": message,
+            "resource": resource,
             "type": "request",
             "request": request,
         }
@@ -270,17 +205,17 @@ class Logger:
         except Exception as e:
             print(str(e))
 
-    def log_request(self, request, resource, action):
+    def log_request(self, request, resource, action, user):
         if resource == Logger.USERS:
-            log_message = self.__log_user_request(request, action)
+            log_message = self.__log_user_request(action)
         if resource == Logger.MODELS:
-            log_message = self.__log_model_request(request, action)
+            log_message = self.__log_model_request(action)
         if resource == Logger.INSTANCES:
-            log_message = self.__log_instance_request(request, action)
+            log_message = self.__log_instance_request(action)
         if resource == Logger.RACKS:
-            log_message = self.__log_rack_request(request, action)
+            log_message = self.__log_rack_request(action)
 
-        self.__create_log_entry_request(request, log_message)
+        self.__create_log_entry_request(request, resource, log_message, user)
 
         return None
 
@@ -295,6 +230,3 @@ class Logger:
         self.__refresh()
 
         return self.LOG
-
-
-print(Actions.RACKS.CREATE)

93bb8e23b652a5bf5c64ea2bd9aa190287aaa382
Eric Werbel <enw12@duke.edu>
2020-02-20 23:05:52 -0500

    updated model network ports to accept array of strings

--- ReactAndFlask/flask-backend/app/models/model_validator.py
+++ ReactAndFlask/flask-backend/app/models/model_validator.py
@@ -11,15 +11,25 @@ class ModelValidator:
         self.instance_table = InstanceTable()
 
     def _validate(self, model: Model) -> str:
+        result = self.model_table.get_model_id_by_vendor_number(
+            model.vendor, model.model_number
+        )
+
+        if result is not None:
+            return "This vendor and model number combination already exists."
+
         pattern = re.compile("[0-9]+")
         if pattern.fullmatch(str(model.height)) is None:
             return "The value for model height must be a positive integer."
-        if (
-            model.ethernet_ports != ""
-            and model.ethernet_ports != None
-            and pattern.fullmatch(str(model.ethernet_ports)) is None
-        ):
-            return "The value for ethernet ports must be a positive integer."
+
+        if model.ethernet_ports is not None:
+            port_pattern = re.compile("[a-zA-Z0-9]+")
+            for port_name in model.ethernet_ports:
+                if port_pattern.fullmatch(port_name) is None:
+                    return "Network port names must not have spaces and only contain letters and numbers"
+                if len(port_name) > 20:
+                    return "Netowrk port names must be shorter than 20 characters."
+
         if (
             model.power_ports != ""
             and model.power_ports != None
@@ -36,13 +46,6 @@ class ModelValidator:
         return "success"
 
     def create_model_validation(self, model: Model) -> str:
-        result = self.model_table.get_model_id_by_vendor_number(
-            model.vendor, model.model_number
-        )
-
-        if result is not None:
-            return "This vendor and model number combination already exists."
-
         return self._validate(model=model)
 
     def edit_model_validation(self, model: Model) -> str:

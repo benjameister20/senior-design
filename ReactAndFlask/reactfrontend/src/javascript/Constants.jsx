89a2eda710c4004937941a0bff2931ee3719ab15
Trevor Stevenson <trevor.stevenson@duke.edu>
2020-03-04 00:42:27 -0500

    Add circleci yml

--- ReactAndFlask/reactfrontend/src/javascript/Constants.jsx
+++ ReactAndFlask/reactfrontend/src/javascript/Constants.jsx
@@ -7,7 +7,7 @@ export const testServerEndpoint = 'https://parseltongue-finishinge-lfw68m.heroku
 export const devServerEndpoint = 'https://parseltongue-dev.herokuapp.com/';
 export const prodServerEndpoint = 'https://parseltongue-prod.herokuapp.com/';
 
-export const serverEndpoint = prodServerEndpoint;
+export const serverEndpoint = localServerEndpoint;
 
 
 export const RackX = [

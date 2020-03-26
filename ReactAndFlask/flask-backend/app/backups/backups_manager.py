c26152420d98f6e427f59d501762394da70ac60e
Connor Ghazaleh <cfg11@duke.edu>
2020-03-26 11:43:43 -0400

    fixing bug to stop storing backups on app server

--- ReactAndFlask/flask-backend/app/backups/backups_manager.py
+++ ReactAndFlask/flask-backend/app/backups/backups_manager.py
@@ -27,6 +27,10 @@ class BackupsManager:
         return self.AM.encrypt_pw(pw)
 
     def generate_backup(self):
+        # Remove all files in the backups directory
+        files = os.listdir(BackupsManager.FILE_PATH)
+        for f in files:
+            os.remove(f)
         # command = f"pg_dump {Constants.BACKUPS_DB} -O -F t > "
         # command = f"pg_dump -d {Constants.BACKUPS_DB} -h {Constants.BACKUP_HOST} -p {Constants.BACKUP_PORT} -U {Constants.BACKUP_USER} -W {Constants.BACKUP_PASS} -O -F t > "
         command = f"pg_dump --dbname=postgresql://{Constants.BACKUPS_USER}:{Constants.BACKUPS_PASS}@{Constants.BACKUPS_HOST}:{Constants.BACKUPS_PORT}/{Constants.BACKUPS_DB} -O -F t > "

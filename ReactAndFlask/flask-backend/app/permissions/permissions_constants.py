5b0515a660e276dbdf70e4f4a72f175438e49084
Ben Lawrence <bcl19@duke.edu>
2020-03-28 14:07:46 -0400

    Changed backend permission constants to be lower case

--- ReactAndFlask/flask-backend/app/permissions/permissions_constants.py
+++ ReactAndFlask/flask-backend/app/permissions/permissions_constants.py
@@ -1,7 +1,7 @@
 class PermissionConstants:
-    MODEL = "Model"
-    ASSET = "Asset"
-    POWER = "Power"
-    AUDIT = "Audit"
-    ADMIN = "Admin"
-    DATACENTERS = "Datacenters"
+    MODEL = "model"
+    ASSET = "asset"
+    POWER = "power"
+    AUDIT = "audit"
+    ADMIN = "admin"
+    DATACENTERS = "datacenters"

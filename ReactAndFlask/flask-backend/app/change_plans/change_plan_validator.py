fc7003e0255038c58aaf7968c8b1a9a4f613f39b
Eric Werbel <enw12@duke.edu>
2020-03-29 14:08:33 -0400

    Added power connection validation and refactored code

--- ReactAndFlask/flask-backend/app/change_plans/change_plan_validator.py
+++ ReactAndFlask/flask-backend/app/change_plans/change_plan_validator.py
@@ -23,7 +23,14 @@ class ChangePlanValidator:
         self.instance_manager = InstanceManager()
         self.rack_height = 42
 
+        self.cp_asset_set = set()
+        self.decom_asset_set = set()
+        self.no_pow_conflict = False
+
     def validate_action(self, cp_action, all_cp_actions):
+        self.cp_asset_set = set()
+        self.decom_asset_set = set()
+        self.no_pow_conflict = False
         cp_validation_result = self._validate_change_plan(cp_action.change_plan_id)
         if cp_validation_result != Constants.API_SUCCESS:
             return cp_validation_result
@@ -63,118 +70,43 @@ class ChangePlanValidator:
         if instance_top > self.rack_height:
             return "The placement of the instance exceeds the height of the rack."
 
-        cp_asset_set = set()
-        decom_asset_set = set()
-
         new_asset_number = instance.asset_number
         new_hostname = instance.hostname
-        for prev_action in all_cp_actions:
-            if prev_action.step >= cp_action.step:
-                continue
+        pow_connections = set(instance.power_connections)
 
-            if prev_action.action != Constants.CREATE_KEY:
-                cp_asset_set.add(prev_action.original_asset_number)
-            if prev_action.action == Constants.DECOMMISSION_KEY:
-                decom_asset_set.add(prev_action.original_asset_number)
-
-            # Asset Number Validation
-            if (
-                prev_action.original_asset_number == new_asset_number
-                and prev_action.action != Constants.DECOMMISSION_KEY
-            ):
-                return f"Asset numbers must be unique. An asset with asset number {instance.asset_number} already exists."
-            if (
-                prev_action.new_record.get(Constants.ASSET_NUMBER_KEY)
-                == new_asset_number
-                and prev_action.action == Constants.CREATE_KEY
-            ):
-                return f"Asset with asset number {new_asset_number} is already being created in step {prev_action.step} of the change plan."
-
-            # Hostname Validation
-            if new_hostname is not None and new_hostname != "":
-                if (
-                    prev_action.new_record.get(Constants.HOSTNAME_KEY) == new_hostname
-                    and prev_action.action != Constants.DECOMMISSION_KEY
-                ):
-                    return f"Asset with hostname {new_hostname} is already exists in step {prev_action.step} of the change plan."
-
-            # Location Validation
-            if (
-                prev_action.action == Constants.CREATE_KEY
-                or prev_action.action == Constants.UPDATE_KEY
-            ):
-                model_id = self.instance_manager.get_model_id_from_name(
-                    prev_action.new_record.get(Constants.MODEL_KEY)
-                )
-                model = self.model_table.get_model(model_id)
-                other_bottom = int(
-                    prev_action.new_record.get(Constants.RACK_POSITION_KEY)
-                )
-                other_top = (
-                    int(prev_action.new_record.get(Constants.RACK_POSITION_KEY))
-                    + model.height
-                    - 1
-                )
-                if other_bottom >= instance_bottom and other_bottom <= instance_top:
-                    result = f"The asset placement conflicts with asset with asset number {prev_action.new_record.get(Constants.ASSET_NUMBER_KEY)} "
-                    result += f"edited in step {prev_action.step}."
-                    return result
-                elif other_top >= instance_bottom and other_top <= instance_top:
-                    result = f"The asset placement conflicts with asset with asset number {prev_action.new_record.get(Constants.ASSET_NUMBER_KEY)} "
-                    result += f"edited in step {prev_action.step}."
-                    return result
-
-        if (
-            self.instance_table.get_instance_by_asset_number(instance.asset_number)
-            is not None
-            and not instance.asset_number in decom_asset_set
-        ):
-            return f"Asset numbers must be unique. An asset with asset number {instance.asset_number} already exists."
-
-        if instance.hostname != "" and instance.hostname is not None:
-            duplicate_hostname = self.instance_table.get_instance_by_hostname(
-                instance.hostname
-            )
-
-            if (
-                duplicate_hostname is not None
-                and not duplicate_hostname.asset_number in cp_asset_set
-            ):
-                return f"An instance with hostname {duplicate_hostname.hostname} exists at location {duplicate_hostname.rack_label} U{duplicate_hostname.rack_position}"
-
-        instance_list = self.instance_table.get_instances_by_rack(
-            instance.rack_label, instance.datacenter_id
+        prev_action_val_result = self._validate_vs_prev_actions(
+            instance,
+            all_cp_actions,
+            cp_action,
+            new_asset_number,
+            new_hostname,
+            pow_connections,
+            instance_bottom,
+            instance_top,
         )
-        if instance_list is not None:
-            for current_instance in instance_list:
-                if current_instance.asset_number in cp_asset_set:
-                    continue
+        if prev_action_val_result != Constants.API_SUCCESS:
+            return prev_action_val_result
 
-                model = self.model_table.get_model(instance.model_id)
-                current_instance_top = current_instance.rack_position + model.height - 1
-                if (
-                    current_instance.rack_position >= instance_bottom
-                    and current_instance.rack_position <= instance_top
-                ):
-                    return self.return_conflict(current_instance)
-                elif (
-                    current_instance_top >= instance_bottom
-                    and current_instance_top <= instance_top
-                ):
-                    return self.return_conflict(current_instance)
+        common_val_result = self._common_validations(
+            instance, cp_action, instance_bottom, instance_top
+        )
+        if common_val_result != Constants.API_SUCCESS:
+            return common_val_result
 
-        # for p_connection in instance.power_connections:
-        #     char1 = p_connection[0].upper()
-        #     num = int(p_connection[1:])
-        #     if char1 == "L":
-        #         pdu_arr = rack.pdu_left
-        #     elif char1 == "R":
-        #         pdu_arr = rack.pdu_right
-        #     else:
-        #         return "Invalid power connection. Please specify left or right PDU."
+        if not self.no_pow_conflict:
+            rack = self.rack_table.get_rack(instance.rack_label, instance.datacenter_id)
+            for p_connection in instance.power_connections:
+                char1 = p_connection[0].upper()
+                num = int(p_connection[1:])
+                if char1 == "L":
+                    pdu_arr = rack.pdu_left
+                elif char1 == "R":
+                    pdu_arr = rack.pdu_right
+                else:
+                    return "Invalid power connection. Please specify left or right PDU."
 
-        #     if pdu_arr[num - 1] == 1:
-        #         return f"There is already an asset connected at PDU {char1}{num}. Please pick an empty PDU port."
+                if pdu_arr[num - 1] == 1:
+                    return f"There is already an asset connected at PDU {char1}{num}. Please pick an empty PDU port."
 
         # connection_validation_result = self.validate_connections(
         #     instance.network_connections, instance.hostname
@@ -191,6 +123,16 @@ class ChangePlanValidator:
         if input_validation_result != Constants.API_SUCCESS:
             return input_validation_result
 
+        model_template = self.model_table.get_model(instance.model_id)
+        if model_template is None:
+            return "The model does not exist."
+
+        instance_bottom = int(instance.rack_position)
+        instance_top = instance_bottom + int(model_template.height) - 1
+
+        if instance_top > self.rack_height:
+            return "The placement of the instance exceeds the height of the rack."
+
         prev_update_in_plan = self.cp_action_table.get_newest_asset_record_in_plan(
             cp_action.change_plan_id, cp_action.original_asset_number, 999999999,
         )
@@ -199,6 +141,29 @@ class ChangePlanValidator:
             and prev_update_in_plan.action != Constants.COLLATERAL_KEY
         ):
             return f"This asset is already being modified in step {prev_update_in_plan.step}. Please update your desired information there."
+
+        new_asset_number = instance.asset_number
+        new_hostname = instance.hostname
+        pow_connections = set(instance.power_connections)
+
+        prev_action_val_result = self._validate_vs_prev_actions(
+            instance,
+            all_cp_actions,
+            cp_action,
+            new_asset_number,
+            new_hostname,
+            pow_connections,
+            instance_bottom,
+            instance_top,
+        )
+        if prev_action_val_result != Constants.API_SUCCESS:
+            return prev_action_val_result
+
+        common_val_result = self._common_validations(
+            instance, cp_action, instance_bottom, instance_top
+        )
+        if common_val_result != Constants.API_SUCCESS:
+            return common_val_result
 
         return Constants.API_SUCCESS
 
@@ -246,6 +211,147 @@ class ChangePlanValidator:
 
         if instance.owner != "" and self.user_table.get_user(instance.owner) is None:
             return f"The owner {instance.owner} is not an existing user. Please enter the username of an existing user."
+
+        return Constants.API_SUCCESS
+
+    def _validate_vs_prev_actions(
+        self,
+        instance,
+        all_cp_actions,
+        cp_action,
+        new_asset_number,
+        new_hostname,
+        pow_connections,
+        instance_bottom: int,
+        instance_top: int,
+    ):
+        for prev_action in all_cp_actions:
+            if prev_action.step >= cp_action.step:
+                continue
+
+            if prev_action.action != Constants.CREATE_KEY:
+                self.cp_asset_set.add(prev_action.original_asset_number)
+            if prev_action.action == Constants.DECOMMISSION_KEY:
+                self.decom_asset_set.add(prev_action.original_asset_number)
+
+            # Asset Number Validation
+            if (
+                prev_action.original_asset_number == new_asset_number
+                and prev_action.action != Constants.DECOMMISSION_KEY
+            ):
+                return f"Asset numbers must be unique. An asset with asset number {instance.asset_number} already exists."
+            if (
+                prev_action.new_record.get(Constants.ASSET_NUMBER_KEY)
+                == new_asset_number
+                and prev_action.action == Constants.CREATE_KEY
+            ):
+                return f"Asset with asset number {new_asset_number} is already being created in step {prev_action.step} of the change plan."
+
+            # Hostname Validation
+            if new_hostname is not None and new_hostname != "":
+                if (
+                    prev_action.new_record.get(Constants.HOSTNAME_KEY) == new_hostname
+                    and prev_action.action != Constants.DECOMMISSION_KEY
+                ):
+                    return f"Asset with hostname {new_hostname} already exists in step {prev_action.step} of the change plan."
+
+            # Location Validation
+            if (
+                prev_action.action == Constants.CREATE_KEY
+                or prev_action.action == Constants.UPDATE_KEY
+            ):
+                model_id = self.instance_manager.get_model_id_from_name(
+                    prev_action.new_record.get(Constants.MODEL_KEY)
+                )
+                model = self.model_table.get_model(model_id)
+                other_bottom = int(
+                    prev_action.new_record.get(Constants.RACK_POSITION_KEY)
+                )
+                other_top = (
+                    int(prev_action.new_record.get(Constants.RACK_POSITION_KEY))
+                    + model.height
+                    - 1
+                )
+                if other_bottom >= instance_bottom and other_bottom <= instance_top:
+                    result = f"The asset placement conflicts with asset with asset number {prev_action.new_record.get(Constants.ASSET_NUMBER_KEY)} "
+                    result += f"edited in step {prev_action.step}."
+                    return result
+                elif other_top >= instance_bottom and other_top <= instance_top:
+                    result = f"The asset placement conflicts with asset with asset number {prev_action.new_record.get(Constants.ASSET_NUMBER_KEY)} "
+                    result += f"edited in step {prev_action.step}."
+                    return result
+
+            # POwer Connection Validation
+            if (
+                prev_action.action != Constants.DECOMMISSION_KEY
+                and prev_action.action != Constants.COLLATERAL_KEY
+            ):
+                prev_pow_connections = prev_action.new_record.get(
+                    Constants.POWER_CONNECTIONS_KEY
+                )
+                prev_pow_connections = set(prev_pow_connections)
+                pow_intersection = pow_connections.instersects(prev_pow_connections)
+                if len(pow_intersection) > 0:
+                    return f"There is already an asset connected at PDU port {pow_intersection.pop()}. Please pick an empty PDU port."
+
+            if (
+                prev_action.action == Constants.UPDATE_KEY
+                or prev_action.action == Constants.DECOMMISSION_KEY
+            ):
+                old_pow_connections = prev_action.old_record.get(
+                    Constants.POWER_CONNECTIONS_KEY
+                )
+                old_pow_connections = set(old_pow_connections)
+                old_pow_intersection = pow_connections.instersects(old_pow_connections)
+                if len(old_pow_intersection) > 0:
+                    self.no_pow_conflict = True
+
+        return Constants.API_SUCCESS
+
+    def _common_validations(self, instance, cp_action, instance_bottom, instance_top):
+        if (
+            self.instance_table.get_instance_by_asset_number(instance.asset_number)
+            is not None
+            and not instance.asset_number in self.decom_asset_set
+            and instance.asset_number != cp_action.original_asset_number
+        ):
+            return f"Asset numbers must be unique. An asset with asset number {instance.asset_number} already exists."
+
+        if instance.hostname != "" and instance.hostname is not None:
+            duplicate_hostname = self.instance_table.get_instance_by_hostname(
+                instance.hostname
+            )
+
+            if (
+                duplicate_hostname is not None
+                and not duplicate_hostname.asset_number in self.cp_asset_set
+                and duplicate_hostname.asset_number != instance.asset_number
+            ):
+                return f"An instance with hostname {duplicate_hostname.hostname} exists at location {duplicate_hostname.rack_label} U{duplicate_hostname.rack_position}"
+
+        instance_list = self.instance_table.get_instances_by_rack(
+            instance.rack_label, instance.datacenter_id
+        )
+        if instance_list is not None:
+            for current_instance in instance_list:
+                if (
+                    current_instance.asset_number in self.cp_asset_set
+                    or current_instance.asset_number == cp_action.original_asset_number
+                ):
+                    continue
+
+                model = self.model_table.get_model(instance.model_id)
+                current_instance_top = current_instance.rack_position + model.height - 1
+                if (
+                    current_instance.rack_position >= instance_bottom
+                    and current_instance.rack_position <= instance_top
+                ):
+                    return self.return_conflict(current_instance)
+                elif (
+                    current_instance_top >= instance_bottom
+                    and current_instance_top <= instance_top
+                ):
+                    return self.return_conflict(current_instance)
 
         return Constants.API_SUCCESS
 

3a8d398a9a4ea37a75cc4b03c3d16186db25fe4a
Ben Lawrence <bcl19@duke.edu>
2020-03-30 00:53:10 -0500

    Don't reset the privileges so that default values stay

--- ReactAndFlask/reactfrontend/src/javascript/users/helpers/CreateUser.jsx
+++ ReactAndFlask/reactfrontend/src/javascript/users/helpers/CreateUser.jsx
@@ -43,7 +43,7 @@ export default class CreateModal extends React.Component {
 
     resetCreate = (success) => {
         if (success) {
-            this.setState({username: "", display_name: "", password: "", email: "",});
+
         }
     }
 

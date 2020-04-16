2d944f7d7f688b9f7e601dc504b6e603e75c1c52
Ben Lawrence <bcl19@duke.edu>
2020-04-16 14:35:42 -0500

    Fixed bug where stats page didn't load the stats when page first opens Removed generate report button Fixed bug where there was no loading icon for stats page

--- ReactAndFlask/reactfrontend/src/javascript/statistics/pages/StatisticsView.jsx
+++ ReactAndFlask/reactfrontend/src/javascript/statistics/pages/StatisticsView.jsx
@@ -9,12 +9,12 @@ import JSONtoArr from "../../helpers/functions/JSONtoArr";
 import StatusDisplay from '../../helpers/StatusDisplay';
 import ErrorBoundray from '../../errors/ErrorBoundry';
 import { Select, MenuItem, InputLabel, Grid, Paper, Typography, Button } from '@material-ui/core';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@material-ui/core';
-import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails } from '@material-ui/core';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
+import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, CircularProgress } from '@material-ui/core';
 
 const statsMainPath = 'stats/';
 const tables = {
-    "totalUsage":"Total Usage",
+    "totalUsage": "Total Usage",
     "spaceUsage": "Space Usage",
     "vendorUsage": "Vendor Usage",
     "modelUsage": "Model Usage",
@@ -22,7 +22,7 @@ const tables = {
 }
 
 const tableCols = {
-    "totalUsage":["Total", "Usage"],
+    "totalUsage": ["Total", "Usage"],
     "spaceUsage": ["Rack", "Usage"],
     "vendorUsage": ["Vendor", "Usage"],
     "modelUsage": ["Model", "Usage"],
@@ -38,25 +38,24 @@ export default class StatisticsView extends React.Component {
             datacenterList: [],
             selectedDatacenter: 'All Datacenters',
             tableValues: {
-                "totalUsage":[],
+                "totalUsage": [],
                 "spaceUsage": [],
                 "vendorUsage": [],
                 "modelUsage": [],
                 "ownerUsage": [],
             },
-            totalUsage:[],
-            spaceUsage:[],
-            vendorUsage:[],
-            modelUsage:[],
-            ownerUsage:[],
+            totalUsage: [],
+            spaceUsage: [],
+            vendorUsage: [],
+            modelUsage: [],
+            ownerUsage: [],
 
-            showStatus:false,
-            statusMessage:'',
-            statusSeverity:'',
+            showStatus: false,
+            statusMessage: '',
+            statusSeverity: '',
         };
 
         this.closeShowStatus = this.closeShowStatus.bind(this);
-        this.generateReport = this.generateReport.bind(this);
 
         axios.defaults.headers.common['token'] = this.props.token;
         axios.defaults.headers.common['privilege'] = this.props.privilege;
@@ -64,6 +63,13 @@ export default class StatisticsView extends React.Component {
 
     componentDidMount() {
         this.getDatacenters();
+
+        var event = {
+            target: {
+                value:"All Datacenters",
+            }
+        }
+        this.generateReport(event);
     }
 
     getDatacenters = () => {
@@ -75,50 +81,54 @@ export default class StatisticsView extends React.Component {
         );
     }
 
-    generateReport() {
+    generateReport = (event) => {
+        var dc = event.target.value;
+        this.setState({ selectedDatacenter: dc, generatingReport: true });
         axios.post(getURL(statsMainPath, StatsCommand.GENERATE_REPORT), {
-            'datacenter_name': this.state.selectedDatacenter.name === "All Datacenters" ? "" : this.state.selectedDatacenter.name,
+            'datacenter_name': dc.name === "All Datacenters" ? "" : dc.name,
         }).then(response => {
-                try {
-                    var data = response.data;
-                    var totalUsage = [];
-                    var totalUsageRow = ["Total Usage"];
-                    totalUsageRow.push(data["totalUsage"]);
-                    totalUsage.push(totalUsageRow);
+            try {
+                var data = response.data;
+                var totalUsage = [];
+                var totalUsageRow = ["Total Usage"];
+                totalUsageRow.push(data["totalUsage"]);
+                totalUsage.push(totalUsageRow);
 
-                    var spaceUsage = JSONtoArr(data["spaceUsage"]);
-                    if (spaceUsage.length === 0) {
-                        spaceUsage.push(["No space is currently being used", []])
-                    }
-                    var vendorUsage = JSONtoArr(data["vendorUsage"]);
-                    if (vendorUsage.length === 0) {
-                        vendorUsage.push(["No vendors currently using space", []])
-                    }
-                    var modelUsage = JSONtoArr(data["modelUsage"]);
-                    if (modelUsage.length === 0) {
-                        modelUsage.push(["No models currently using space", []])
-                    }
-                    var ownerUsage = JSONtoArr(data["ownerUsage"]);
-                    if (ownerUsage.length === 0) {
-                        ownerUsage.push(["No owners currently using space", []])
-                    }
-
-                    this.setState({
-                        showStatus: true,
-                        statusSeverity:"success",
-                        statusMessage: "Success",
-                        tableValues: {
-                            "totalUsage":totalUsage,
-                            "spaceUsage": spaceUsage,
-                            "vendorUsage": vendorUsage,
-                            "modelUsage": modelUsage,
-                            "ownerUsage": ownerUsage,
-                        },
-                     })
-                } catch {
-                    this.setState({ showStatus: true, statusMessage: response.data.message, statusSeverity:"error" })
+                var spaceUsage = JSONtoArr(data["spaceUsage"]);
+                if (spaceUsage.length === 0) {
+                    spaceUsage.push(["No space is currently being used", []])
                 }
-            });
+                var vendorUsage = JSONtoArr(data["vendorUsage"]);
+                if (vendorUsage.length === 0) {
+                    vendorUsage.push(["No vendors currently using space", []])
+                }
+                var modelUsage = JSONtoArr(data["modelUsage"]);
+                if (modelUsage.length === 0) {
+                    modelUsage.push(["No models currently using space", []])
+                }
+                var ownerUsage = JSONtoArr(data["ownerUsage"]);
+                if (ownerUsage.length === 0) {
+                    ownerUsage.push(["No owners currently using space", []])
+                }
+
+                this.setState({
+                    showStatus: true,
+                    statusSeverity: "success",
+                    statusMessage: "Successfully generated Report",
+                    tableValues: {
+                        "totalUsage": totalUsage,
+                        "spaceUsage": spaceUsage,
+                        "vendorUsage": vendorUsage,
+                        "modelUsage": modelUsage,
+                        "ownerUsage": ownerUsage,
+                    },
+                })
+            } catch {
+                this.setState({ showStatus: true, statusMessage: "Successfully generated Report", statusSeverity: "error" })
+            }
+
+            this.setState({ generatingReport: false })
+        });
     }
 
     closeShowStatus() {
@@ -129,81 +139,76 @@ export default class StatisticsView extends React.Component {
         return (
             <div>
                 <ErrorBoundray >
-                <StatusDisplay
-                    open={this.state.showStatus}
-                    severity={this.state.statusSeverity}
-                    closeStatus={this.closeShowStatus}
-                    message={this.state.statusMessage}
-                />
-                <Grid
-                    container
-                    spacing={5}
-                    direction="row"
-                    justify="center"
-                    alignItems="center"
-                    style={{margin: "0px", maxWidth: "95vw"}}
-                >
-                    <Grid item xs={12}>
-                        <Typography variant="h4">
-                            Reports
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <InputLabel id="datacenter-select-label">Select Datacenter</InputLabel>
-                        <Select
-                            name='datacenter_name'
-                            id="datacenter-select"
-                            value={this.state.selectedDatacenter}
-                            onChange={(e) => this.setState({ selectedDatacenter: e.target.value })}
-                            style={{ width: "100%" }}
-                        >
-                            <MenuItem value="All Datacenters">All Datacenters</MenuItem>
-                            {this.state.datacenterList.map(value => {
-                                return (<MenuItem value={value}>{value["name"]}</MenuItem>);
-                            })}
-                        </Select>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={this.generateReport}
-                            disabled={this.state.loadingDatacenters}
-                        >
-                            Generate Report
-                        </Button>
-                    </Grid>
-                </Grid>
-                <div>
-
-                </div>
-                {Object.keys(tables).map(key => (
-                <ExpansionPanel>
-                    <ExpansionPanelSummary
-                        expandIcon={<ExpandMoreIcon />}
+                    <StatusDisplay
+                        open={this.state.showStatus}
+                        severity={this.state.statusSeverity}
+                        closeStatus={this.closeShowStatus}
+                        message={this.state.statusMessage}
+                    />
+                    <Grid
+                        container
+                        spacing={5}
+                        direction="row"
+                        justify="center"
+                        alignItems="center"
+                        style={{ margin: "0px", maxWidth: "95vw" }}
                     >
-                        <Typography>{tables[key]}</Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails>
-                        <TableContainer component={Paper}>
-                            <Table>
-                                <TableHead>
-                                    <TableRow >
-                                        {tableCols[key].map(column => (<TableCell><span id={column}>{column}</span></TableCell>))}
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                {this.state.tableValues[key].map(row => (
-                                    <TableRow>
-                                        {row.map(column => (<TableCell><span id={column}>{column + (isNaN(column) ? "" : "%")}</span></TableCell>))}
-                                    </TableRow>
-                                ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </ExpansionPanelDetails>
-                </ExpansionPanel>
-                ))}
+                        <Grid item xs={12}>
+                            <Typography variant="h4">
+                                Reports
+                        </Typography>
+                        </Grid>
+                        <Grid item xs={3}>
+                            {this.state.loadingDatacenters || this.state.generatingReport ?
+                                <CircularProgress /> :
+                                <span>
+                                    <InputLabel id="datacenter-select-label">Select Datacenter</InputLabel>
+                                    <Select
+                                        name='datacenter_name'
+                                        id="datacenter-select"
+                                        value={this.state.selectedDatacenter}
+                                        onChange={(event) => this.generateReport(event)}
+                                        style={{ width: "100%" }}
+                                    >
+                                        <MenuItem value="All Datacenters">All Datacenters</MenuItem>
+                                        {this.state.datacenterList.map(value => {
+                                            return (<MenuItem value={value}>{value["name"]}</MenuItem>);
+                                        })}
+                                    </Select>
+                                </span>
+                            }
+                        </Grid>
+                    </Grid>
+                    <div>
+
+                    </div>
+                    {Object.keys(tables).map(key => (
+                        <ExpansionPanel>
+                            <ExpansionPanelSummary
+                                expandIcon={<ExpandMoreIcon />}
+                            >
+                                <Typography>{tables[key]}</Typography>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <TableContainer component={Paper}>
+                                    <Table>
+                                        <TableHead>
+                                            <TableRow >
+                                                {tableCols[key].map(column => (<TableCell><span id={column}>{column}</span></TableCell>))}
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {this.state.tableValues[key].map(row => (
+                                                <TableRow>
+                                                    {row.map(column => (<TableCell><span id={column}>{column + (isNaN(column) ? "" : "%")}</span></TableCell>))}
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
+                    ))}
                 </ErrorBoundray>
             </div>
         );

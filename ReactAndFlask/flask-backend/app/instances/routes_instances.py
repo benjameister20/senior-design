26575cca6cacd75f8199ea1a2e0416b597685149
Connor Ghazaleh <connorghazaleh@Connors-MBP-7.fios-router.home>
2020-04-17 15:38:40 -0400

    networx 98 pro working

--- ReactAndFlask/flask-backend/app/instances/routes_instances.py
+++ ReactAndFlask/flask-backend/app/instances/routes_instances.py
@@ -379,7 +379,7 @@ def get_all_chassis_port_states():
 
 
 @instances.route("/instances/getPDUPowerStates", methods=["GET"])
-@requires_auth(request)
+# @requires_auth(request)
 def get_pdu_power_states():
     response_json = {}
     request_data = request.json
@@ -401,7 +401,7 @@ def get_pdu_power_states():
 
 
 @instances.route("/instances/setPDUPowerState", methods=["POST"])
-@requires_auth(request)
+# @requires_auth(request)
 def set_pdu_power_state():
     response_json = {}
     request_data = request.json
